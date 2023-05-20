9dbcee83b08099b795b5d4e14b6d8fd1d5063e6c
Akinori99 <akinori.work99@gmail.com>
2023-05-20 20:06:59 +0900

    Add ScssFiles & Update index.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
+// import { defineNuxtConfig } from "nuxt";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  css: ["@/assets/styles/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/styles/_variables.scss";`,
+        },
+      },
+    },
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>IndexPage</h1>
+  <div class="wrapper">
+    <div class="title">
+      <h1>Akinori's<br />Portfolio</h1>
+    </div>
+    <router-link to="/works" class="btn"> 作品を見る </router-link>
+
+    <div class="top-contact">
+      <!-- ↓↓ index.vue<contact>と以下の部分をコンポーネント化・置換 -->
+      <div class="contact-logo">
+        <a href="https://github.com/Akinori99"
+          ><img src="~assets/images/github.png" alt="GitHub"
+        /></a>
+        <a href="https://twitter.com/Akinori_99?ref_src=twsrc%5Etfw"
+          ><img src="~assets/images/twitter.png" alt="Twitter"
+        /></a>
+      </div>
+      <!-- ↑↑ -->
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  layout: "index",
+};
+</script>
+
+<style lang="scss"></style>
