e789513db5e9a18bc82fa6ac48b3d9d78edb5680
Akinori99 <akinori.work99@gmail.com>
2023-05-24 20:39:29 +0900

    Add componentFiles

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <!-- ↓↓ Dateオブジェクトの.getYearメソッドを用いて表示させる -->
+    <p>&copy; 2021 AbeAkinori</p>
+    <!-- ↑↑ -->
+  </footer>
+</template>
+
+<style lang="scss">
+footer {
+  display: flex;
+  align-items: center;
+  position: absolute;
+  background: rgba(61, 47, 33, 0.6);
+  height: 30px;
+  margin: 0 auto;
+  p {
+    color: azure;
+    font-size: 0.9rem;
+  }
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,171 @@
+<template>
+
+  <header>
+    <v-app-bar
+      app
+      fixed
+      dark
+      hide-on-scroll
+      height="55px"
+    >
+    <!-- ↓↓ ロゴ画像とタイトルをクリックしたらホーム遷移するようリンク付 -->
+      <img
+        class="mylogo"
+        src="~assets/images/mylogo.svg"
+        alt="mylogo"
+      />
+      <v-toolbar-title>
+        <div class="h-title">Portfolio</div>
+      </v-toolbar-title>
+      <!-- ↑↑ -->
+      <v-tabs>
+        <v-tab
+          v-for="(menuItem, index) in menuItems"
+          :key="index"
+          :to="menuItem.url"
+        >
+          {{ menuItem.name }}
+        </v-tab>
+      </v-tabs>
+      <v-app-bar-nav-icon
+        v-if="drawer === false"
+        id="open"
+        @click="drawer = !drawer"
+      />
+      <v-icon
+        v-else-if="drawer === true"
+        @click="drawer = false"
+      >
+        mdi-close
+      </v-icon>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      temporary
+      width="55%"
+      dark
+      right
+    >
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item
+            v-for="(menuItem, index) in menuItems"
+            :key="index"
+            :to="menuItem.url"
+          >
+            <v-list-item-title>
+              <div>{{ menuItem.name }}</div>
+            </v-list-item-title>
+          </v-list-item>
+          <div class="close" @click="drawer = false">
+            <!-- ↓↓ ×の判定範囲を適切なものに修正 -->
+            <p>✕</p>
+            <!-- ↑↑ -->
+          </div>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </header>
+</template>
+
+<script>
+import constants from '~/assets/constants'
+
+export default {
+  data() {
+    return {
+      drawer: false,
+      menuItems: constants.menuItems,
+    }
+  },
+
+}
+</script>
+
+<style lang="scss">
+// ナビゲーションタブ
+.v-app-bar {
+  max-width: 100vw;
+}
+.mylogo {
+  height: 55px;
+  position: relative;
+  top: 0;
+  margin:0 10px;
+}
+.v-toolbar__title {
+  min-width: fit-content;
+  .h-title {
+    font-family: 'Pinyon Script', cursive;
+    font-size: 40px;
+  }
+}
+.v-tabs-bar__content {
+  z-index: 5;
+  display: block flex;
+  justify-content: flex-end;
+  margin-right: 30px;
+  .v-tab {
+    @include h {
+      display: none;
+    }
+    margin-left: 15px;
+    font-size: 25px;
+    font-weight: bold;
+    letter-spacing: 0.1rem;
+    &:hover {
+      text-decoration: none;
+      color: azure;
+      opacity: 0.6;
+    }
+    &.v-tab--active {
+      color: aqua;
+    }
+  }
+}
+
+// ナビゲーションドロワー
+.v-navigation-drawer {
+  margin-top: 55px;
+  position: fixed;
+}
+.v-list-item__title {
+  & > div {
+    margin: 15px 0;
+    padding: 20px 0;
+    font-size: 25px;
+    font-weight: bold;
+  }
+}
+.v-list-item {
+  position: relative;
+  &:first-child {
+    margin-top: 20px;
+  }
+  &.v-item--active {
+    .v-list-item__title {
+      color: aqua;
+    }
+  }
+}
+#open {
+  display: none;
+  font-size: 3.5rem;
+  margin-left: 10px;
+  @include h {
+    display: block;
+  }
+}
+.close {
+  display: inline-block;
+  position: absolute;
+  right: 0;
+  height: fit-content;
+  color: #ffffff;
+  font-size: 3rem;
+  p {
+    display: inline;
+  }
+}
+</style>
