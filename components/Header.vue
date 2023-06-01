451d372ca9f0db5a6c9227e3883dbd7b32a443ff
Akinori99 <akinori.work99@gmail.com>
2023-06-01 21:11:36 +0900

    Update Header

--- components/Header.vue
+++ components/Header.vue
@@ -6,51 +6,51 @@
           <img class="mylogo" src="~/assets/images/favicon.ico" alt="favicon" />
         </nuxt-link>
       </div>
-    </div>
-    <nav>
-      <button id="menu-open" class="btn-menu">
-        <svg
-          height="24"
-          viewBox="0 0 24 24"
-          width="24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>メニューを開く</title>
-          <path
-            clip-rule="evenodd"
-            d="m4.25 8c0-.41421.33579-.75.75-.75h14c.4142 0 .75.33579.75.75s-.3358.75-.75.75h-14c-.41421 0-.75-.33579-.75-.75zm0 4c0-.4142.33579-.75.75-.75h14c.4142 0 .75.3358.75.75s-.3358.75-.75.75h-14c-.41421 0-.75-.3358-.75-.75zm.75 3.25c-.41421 0-.75.3358-.75.75s.33579.75.75.75h14c.4142 0 .75-.3358.75-.75s-.3358-.75-.75-.75z"
-            fill-rule="evenodd"
-          />
-        </svg>
-      </button>
-      <div id="menu-panel">
-        <button id="menu-close" class="btn-menu">
+      <!-- スライドメニュー -->
+      <nav>
+        <button id="menu-open" class="btn-menu">
           <svg
             height="24"
             viewBox="0 0 24 24"
             width="24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <title>メニューを閉じる</title>
+            <title>メニューを開く</title>
             <path
               clip-rule="evenodd"
-              d="m7.53033 6.46967c-.29289-.29289-.76777-.29289-1.06066 0s-.29289.76777 0 1.06066l4.46963 4.46967-4.46963 4.4697c-.29289.2929-.29289.7677 0 1.0606s.76777.2929 1.06066 0l4.46967-4.4696 4.4697 4.4696c.2929.2929.7677.2929 1.0606 0s.2929-.7677 0-1.0606l-4.4696-4.4697 4.4696-4.46967c.2929-.29289.2929-.76777 0-1.06066s-.7677-.29289-1.0606 0l-4.4697 4.46963z"
+              d="m4.25 8c0-.41421.33579-.75.75-.75h14c.4142 0 .75.33579.75.75s-.3358.75-.75.75h-14c-.41421 0-.75-.33579-.75-.75zm0 4c0-.4142.33579-.75.75-.75h14c.4142 0 .75.3358.75.75s-.3358.75-.75.75h-14c-.41421 0-.75-.3358-.75-.75zm.75 3.25c-.41421 0-.75.3358-.75.75s.33579.75.75.75h14c.4142 0 .75-.3358.75-.75s-.3358-.75-.75-.75z"
               fill-rule="evenodd"
             />
           </svg>
         </button>
-        <ul class="menu-list">
-          <li><nuxt-link to="/">TOP</nuxt-link></li>
-          <li><nuxt-link to="/works">WORKS</nuxt-link></li>
-          <li><nuxt-link to="/about">ABOUT</nuxt-link></li>
-        </ul>
-      </div>
-    </nav>
+        <div id="menu-panel">
+          <button id="menu-close" class="btn-menu">
+            <svg
+              height="24"
+              viewBox="0 0 24 24"
+              width="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>メニューを閉じる</title>
+              <path
+                clip-rule="evenodd"
+                d="m7.53033 6.46967c-.29289-.29289-.76777-.29289-1.06066 0s-.29289.76777 0 1.06066l4.46963 4.46967-4.46963 4.4697c-.29289.2929-.29289.7677 0 1.0606s.76777.2929 1.06066 0l4.46967-4.4696 4.4697 4.4696c.2929.2929.7677.2929 1.0606 0s.2929-.7677 0-1.0606l-4.4696-4.4697 4.4696-4.46967c.2929-.29289.2929-.76777 0-1.06066s-.7677-.29289-1.0606 0l-4.4697 4.46963z"
+                fill-rule="evenodd"
+              />
+            </svg>
+          </button>
+          <ul class="menu-list">
+            <li><nuxt-link to="/">TOP</nuxt-link></li>
+            <li><nuxt-link to="/works">WORKS</nuxt-link></li>
+            <li><nuxt-link to="/about">ABOUT</nuxt-link></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   </header>
 </template>
-
 <script>
-// スライドメニュー
+// ==========スライドメニュー===========
 const menuOpen = document.querySelector("#menu-open");
 const menuClose = document.querySelector("#menu-close");
 const menuPanel = document.querySelector("#menu-panel");
@@ -73,7 +73,7 @@ menuOpen.addEventListener("click", () => {
       },
       {
         duration: 2400,
-        delay: 300 * index,
+        delay: 100 * index,
         easing: "ease",
         fill: "forwards",
       }
@@ -97,8 +97,10 @@ menuClose.addEventListener("click", () => {
   top: 0;
   margin: 0 10px;
 }
-// SLIDE MENU
-// ================================================ */
+
+/*
+SLIDE MENU
+================================================ */
 /* 開閉ボタン */
 .btn-menu {
   position: fixed;
