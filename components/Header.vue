89483a0e44b0f39e39b85c9b4019fe47930e8d31
Akinori99 <akinori.work99@gmail.com>
2023-06-04 20:56:26 +0900

    Update HeaderSlideMenu

--- components/Header.vue
+++ components/Header.vue
@@ -1,105 +1,14 @@
-<!-- <template>
-  <header>
-    <div>
-      <div>
-        <nuxt-link to="/">
-          <img class="mylogo" src="~/assets/images/favicon.ico" alt="favicon" />
-        </nuxt-link>
-      </div>
-      スライドメニュー
-      <nav>
-        <button id="menu-open" class="btn-menu">
-          <svg
-            height="24"
-            viewBox="0 0 24 24"
-            width="24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>メニューを開く</title>
-            <path
-              clip-rule="evenodd"
-              d="m4.25 8c0-.41421.33579-.75.75-.75h14c.4142 0 .75.33579.75.75s-.3358.75-.75.75h-14c-.41421 0-.75-.33579-.75-.75zm0 4c0-.4142.33579-.75.75-.75h14c.4142 0 .75.3358.75.75s-.3358.75-.75.75h-14c-.41421 0-.75-.3358-.75-.75zm.75 3.25c-.41421 0-.75.3358-.75.75s.33579.75.75.75h14c.4142 0 .75-.3358.75-.75s-.3358-.75-.75-.75z"
-              fill-rule="evenodd"
-            />
-          </svg>
-        </button>
-        <div id="menu-panel">
-          <button id="menu-close" class="btn-menu">
-            <svg
-              height="24"
-              viewBox="0 0 24 24"
-              width="24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>メニューを閉じる</title>
-              <path
-                clip-rule="evenodd"
-                d="m7.53033 6.46967c-.29289-.29289-.76777-.29289-1.06066 0s-.29289.76777 0 1.06066l4.46963 4.46967-4.46963 4.4697c-.29289.2929-.29289.7677 0 1.0606s.76777.2929 1.06066 0l4.46967-4.4696 4.4697 4.4696c.2929.2929.7677.2929 1.0606 0s.2929-.7677 0-1.0606l-4.4696-4.4697 4.4696-4.46967c.2929-.29289.2929-.76777 0-1.06066s-.7677-.29289-1.0606 0l-4.4697 4.46963z"
-                fill-rule="evenodd"
-              />
-            </svg>
-          </button>
-          <ul class="menu-list">
-            <li><nuxt-link to="/">TOP</nuxt-link></li>
-            <li><nuxt-link to="/works">WORKS</nuxt-link></li>
-            <li><nuxt-link to="/about">ABOUT</nuxt-link></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  </header>
-</template>
-<script> -->
-<!-- // ==========スライドメニュー===========
-const menuOpen = document.querySelector("#menu-open");
-const menuClose = document.querySelector("#menu-close");
-const menuPanel = document.querySelector("#menu-panel");
-const menuItems = document.querySelectorAll("#menu-panel li");
-const menuOptions = {
-  duration: 1400,
-  easing: "ease",
-  fill: "forwards",
-};
-
-// メニューを開く
-menuOpen.addEventListener("click", () => {
-  menuPanel.animate({ translate: ["100vw", 0] }, menuOptions); -->
-<!-- // リンクをひとつずつ順に表示 -->
-<!-- menuItems.forEach((menuItem, index) => {
-    menuItem.animate(
-      {
-        opacity: [0, 1],
-        translate: ["2rem", 0],
-      },
-      {
-        duration: 2400,
-        delay: 100 * index,
-        easing: "ease",
-        fill: "forwards",
-      }
-    );
-  });
-});
-
-// メニューを閉じる
-menuClose.addEventListener("click", () => {
-  menuPanel.animate({ translate: [0, "100vw"] }, menuOptions);
-  menuItems.forEach((menuItem) => {
-    menuItem.animate({ opacity: [1, 0] }, menuOptions);
-  });
-});
-</script> -->
 <template>
   <header>
     <div>
       <div>
-        <nuxt-link to="/">
+        <nuxt-link to="/" @click="closeMenu">
           <img class="mylogo" src="~/assets/images/favicon.ico" alt="favicon" />
         </nuxt-link>
       </div>
       <!-- スライドメニュー -->
-      <nav ref="menuPanelRef">
-        <button class="btn-menu" @click="openMenu">
+      <nav>
+        <button class="btn-menu" @click="toggleMenu">
           <svg
             height="24"
             viewBox="0 0 24 24"
@@ -114,8 +23,8 @@ menuClose.addEventListener("click", () => {
             />
           </svg>
         </button>
-        <div ref="menuPanelContentRef">
-          <button class="btn-menu" @click="closeMenu">
+        <div class="menu-panel" :class="{ 'menu-panel-open': isMenuOpen }">
+          <button class="btn-menu" @click="toggleMenu">
             <svg
               height="24"
               viewBox="0 0 24 24"
@@ -131,9 +40,9 @@ menuClose.addEventListener("click", () => {
             </svg>
           </button>
           <ul class="menu-list">
-            <li><nuxt-link to="/">TOP</nuxt-link></li>
-            <li><nuxt-link to="/works">WORKS</nuxt-link></li>
-            <li><nuxt-link to="/about">ABOUT</nuxt-link></li>
+            <li><nuxt-link to="/" @click="closeMenu">TOP</nuxt-link></li>
+            <li><nuxt-link to="/works" @click="closeMenu">WORKS</nuxt-link></li>
+            <li><nuxt-link to="/about" @click="closeMenu">ABOUT</nuxt-link></li>
           </ul>
         </div>
       </nav>
@@ -142,47 +51,19 @@ menuClose.addEventListener("click", () => {
 </template>
 
 <script>
-import { ref } from "vue";
-
 export default {
-  mounted() {
-    const menuPanel = this.$refs.menuPanelRef;
-    const menuPanelContent = this.$refs.menuPanelContentRef;
-    const menuItems = menuPanelContent.querySelectorAll("li");
-
-    const menuOptions = {
-      duration: 1400,
-      easing: "ease",
-      fill: "forwards",
+  data() {
+    return {
+      isMenuOpen: false,
     };
-
-    // メニューを開く
-    this.openMenu = () => {
-      menuPanel.animate({ translate: ["100vw", 0] }, menuOptions);
-      // リンクをひとつずつ順に表示
-      menuItems.forEach((menuItem, index) => {
-        menuItem.animate(
-          {
-            opacity: [0, 1],
-            translate: ["2rem", 0],
-          },
-          {
-            duration: 2400,
-            delay: 100 * index,
-            easing: "ease",
-            fill: "forwards",
-          }
-        );
-      });
-    };
-
-    // メニューを閉じる
-    this.closeMenu = () => {
-      menuPanel.animate({ translate: [0, "100vw"] }, menuOptions);
-      menuItems.forEach((menuItem) => {
-        menuItem.animate({ opacity: [1, 0] }, menuOptions);
-      });
-    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    closeMenu() {
+      this.isMenuOpen = false;
+    },
   },
 };
 </script>
@@ -219,15 +100,15 @@ SLIDE MENU
 }
 
 /* 閉じるボタン */
-#menu-close {
+.menu-panel-open .btn-menu {
   border: 1px solid var(--light-green);
 }
-#menu-close svg {
+.menu-panel-open .btn-menu svg {
   fill: var(--light-green);
 }
 
 /* スライドメニューパネル */
-#menu-panel {
+.menu-panel {
   position: fixed;
   top: 0;
   right: 0;
@@ -238,7 +119,11 @@ SLIDE MENU
   background-color: var(--brown);
   box-shadow: 0 0 2rem var(--brown);
   font-family: var(--oswald-font);
-  translate: 100vw;
+  transform: translateX(100%);
+  transition: transform 0.4s;
+}
+.menu-panel-open {
+  transform: translateX(0);
 }
 .menu-list {
   list-style: none;
@@ -246,6 +131,10 @@ SLIDE MENU
 .menu-list li {
   margin: 1.5rem 0;
   opacity: 0;
+  transition: opacity 0.4s;
+}
+.menu-panel-open .menu-list li {
+  opacity: 1;
 }
 .menu-list a {
   color: var(--light-green);
