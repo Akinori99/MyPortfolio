921c3cbf97da72bf81401c87387bebfeb291c459
Akinori99 <akinori.work99@gmail.com>
2023-06-14 13:32:48 +0900

    Setting of nuxt.confing.ts & reflection of favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   css: ["@/assets/styles/main.scss"],
+//   vite: {
+//     css: {
+//       preprocessorOptions: {
+//         scss: {
+//           additionalData: `@import "@/assets/styles/_variables.scss";`,
+//         },
+//       },
+//     },
+//   },
+//   app: {
+//     head: {
+//       title: "Akinori'sPortfolio",
+//       meta: [{ name: "description", content: "Portfolio" }],
+//       link: [{ rel: "icon", href: "~/assets/images/favicon.ico }],
+//     },
+//   },
+// });
+// import { defineNuxtConfig } from "nuxt3";
+
 export default defineNuxtConfig({
   css: ["@/assets/styles/main.scss"],
   vite: {
@@ -8,6 +29,13 @@ export default defineNuxtConfig({
           additionalData: `@import "@/assets/styles/_variables.scss";`,
         },
       },
+    },
+  },
+  app: {
+    head: {
+      title: "Akinori's Portfolio",
+      meta: [{ name: "description", content: "Portfolio" }],
+      link: [{ rel: "icon", href: "/favicon.png" }],
     },
   },
 });
